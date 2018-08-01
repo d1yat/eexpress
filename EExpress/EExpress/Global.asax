32949e0ac877f654104ced15455b3b79427df00f
me.sudiyat <me.sudiyat@gmail.com>
2018-08-01 19:26:49 +0700

    Start creating new ASP.Net MVC project

---
+++ EExpress/EExpress/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EExpress.MvcApplication" Language="C#" %>
---
+++ EExpress/EExpress/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EExpress.Startup))]
+namespace EExpress
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
